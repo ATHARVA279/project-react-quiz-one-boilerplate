337f52154746fe47750fbc5e30fb5655a1c3e10d
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-01-11 12:52:31 +0530

    added

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-// import HomeComponent from './Components/HomeComponent';
-// import QuizComponent from './Components/QuizComponent';
+import HomeComponent from './Components/HomeComponent';
+import QuizComponent from './Components/QuizComponent';
 import ResultComponent from './Components/ResultComponent';
 
 function App() {
